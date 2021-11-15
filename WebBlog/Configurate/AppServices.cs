d451c9247b302294e66f7ee1d6310d70e68d5f6a
mykhailo <michaelh12r2@gmail.com>
2021-11-15 17:25:24 +0200

    setup project and add view components

---
+++ WebBlog/Configurate/AppServices.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebBlog.Data;
+
+namespace WebBlog.Configurate
+{
+    public static class AppServices
+    {
+        public static void AddDefaultServices(this IServiceCollection serviceCollection, IConfiguration configuration)
+        {
+            serviceCollection.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    configuration.GetConnectionString("DefaultConnection")));
+            serviceCollection.AddDatabaseDeveloperPageExceptionFilter();
+
+            serviceCollection.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+            serviceCollection.AddControllersWithViews().AddRazorRuntimeCompilation();
+        }
+    }
+}
